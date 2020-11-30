743ffdc8d7f0f5a64a3b6f9d13e58d074f6e4b7c
Tinh Dz <gaconchaylontonj1@gmai.com>
2020-11-30 23:11:51 +0700

    fileCpp ne

---
+++ tinhxongrc++.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout << "Nhap phan tu cua mang: ";
+	cin >> n;
+	int arr[n];
+	int a = -1;
+	int b = -1;
+	int mangchan[n];
+	int mangle[n];
+	for (int i = 0; i < n; i++){
+		cout << "Nhap phan tu arr[" << i << "]: ";
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++){
+		if (arr[i] % 2 != 0){
+			a++;
+			mangle[a] = arr[i];
+		}else {
+			b++;
+			mangchan[b] = arr[i];
+		}
+	}
+	for (int i = 0; i <= n; i++){
+		mangle[a+1+i] = mangchan[i];
+	}
+	for (int i = 0; i < n; i++){
+		cout << "Arr[" << i << "]: " << mangle[i] << "\t";
+	}
+	
+}
